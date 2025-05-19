924a396657f01564722799b82ef501904115ce2c
Anenqui <anenquipintadovenegas@gmail.com>
2025-05-19 14:31:57 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-red-500">¡Hola Mundo con Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;
